18b4d9cb882c0193a38202cc2f1209fcd35d34ef
dp_PC <ddddreem@qq.com>
2024-08-08 23:06:09 +0800

    states bar display

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	new Vue({
+		el: '#app',
+		data: {
+			todos: [
+				{id: 1, completed: true, title: '示例代办1'},
+				{id: 2, completed: false, title: '示例代办2'},
+				{id: 3, completed: true, title: '示例代办3'}
+			]
+		},
+		computed: {
+			remaining() {
+				return this.todos.filter(todo => !todo.completed).length;
+			}
+		},
+		methods: {
+			pluralize(word) {
+				return word + (this.remaining === 1 ? "" : "s");
+			}
+		}
+	});
 
 })(window);
