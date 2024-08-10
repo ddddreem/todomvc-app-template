495a67557f33d6f34c8e9d4274042a58d760dbc1
dp_PC <ddddreem@qq.com>
2024-08-10 08:40:18 +0800

    todo delete button func completed

--- js/app.js
+++ js/app.js
@@ -14,13 +14,29 @@
 			remaining() {
 				return this.todos.filter(todo => !todo.completed).length;
 			},
-			allDone() {
-				return this.remaining === 0;
+			allDone: {
+				get() {
+					return this.remaining === 0;
+				},
+				set(value) {
+					this.todos.forEach(todo => {
+						todo.completed = value;
+					});
+				}
 			}
 		},
 		methods: {
 			pluralize(word) {
 				return word + (this.remaining === 1 ? "" : "s");
+			},
+			// 用于删除单个代办
+			removeTodo(todo){
+				var index = this.todos.indexOf(todo);
+				this.todos.splice(index, 1);
+			},
+			// 清除所有已完成代办
+			removeCompleted(){
+				this.todos = this.todos.filter( todo => !todo.completed);
 			}
 		}
 	});
